a7327f5fdfe0b3bc6964c051413ef78c4b194731
caballa <navasjorgea@gmail.com>
2017-07-26 13:55:52 -0700

    Added mcmt formar for lfht_2

---
+++ sal/run_sally.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -ne 1 ];then
+    echo "Usage sally.sh FILE"
+    exit 1
+fi
+
+INPUT=$1
+sally --solver y2m5 --engine=pdkind --pdkind-add-backward $INPUT -v 3 --lsal-extensions
--- sal/sally_lfht_1.sh
+++
@@ -1,1 +0,0 @@
-sally --solver y2m5 --engine=pdkind --pdkind-add-backward lfht_1.mcmt -v 3 --lsal-extensions
