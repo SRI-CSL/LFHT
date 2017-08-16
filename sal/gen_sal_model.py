1727834601ebda6cbcc07dc33464aefa61242443
caballa <navasjorgea@gmail.com>
2017-08-16 01:32:59 -0700

    more renaming and cleanup

--- sal/gen_sal_model.py
+++ sal/gen_sal_model.py
@@ -265,18 +265,18 @@ paid_tax_1 : LEMMA
       system |- G( paid_tax >= 0);		
 
 % PROVED
-% sal-inf-bmc -i -ice -v 1 -d 1 -s yices2 {0} revenue_2 
-revenue_2 : LEMMA
+% sal-inf-bmc -i -ice -v 1 -d 1 -s yices2 {0} revenue_1 
+revenue_1 : LEMMA
       system |- G( old < new =>  revenue = Migrated(ht,old));		
 
 % PROVED
-% sal-inf-bmc -i -ice -v 1 -d 1 -s yices2 {0} revenue_3 -l revenue_2
-revenue_3 : LEMMA
+% sal-inf-bmc -i -ice -v 1 -d 1 -s yices2 {0} revenue_2 -l revenue_1
+revenue_2 : LEMMA
       system |- G( old < new => revenue <= ht[old].num_entries);		
 
 % PROVED
-% sal-inf-bmc -i -ice -v 1 -d 1 -s yices2 {0} revenue_4 
-revenue_4 : LEMMA
+% sal-inf-bmc -i -ice -v 1 -d 1 -s yices2 {0} revenue_3 
+revenue_3 : LEMMA
       system |- G( old < new AND ht[old].num_to_migrate > 0 => revenue = T * paid_tax);
 
 % PROVED
@@ -317,13 +317,13 @@ out_of_mem : LEMMA
 %%% END strengthening used for upper lemmas %%%%
 
 % PROVED 
-% sal-inf-bmc -i -ice -v 1 -d {1} -s yices2 {0} upper_global -l pending_lb -l pending -l old_and_new -l empty_beyond_new -l empty_beyond_non_full -l num_to_migrate_and_num_entries -l table_sizes   -l new_cannot_migrate -l old_is_full -l not_full_cannot_be_assimilated -l assimilated_nothing_to_migrate -l one_thread_at_init_state_after_migration -l upper_strengthening_1 -l upper_strengthening_2 -l paid_tax_0 -l paid_tax_1 -l posted_0 -l posted_1 -l did_not_pay_ub  -l upper_strengthening_3  -l out_of_mem -l revenue_2 -l revenue_3 -l revenue_4 
+% sal-inf-bmc -i -ice -v 1 -d {1} -s yices2 {0} upper_global -l pending_lb -l pending -l old_and_new -l empty_beyond_new -l empty_beyond_non_full -l num_to_migrate_and_num_entries -l table_sizes   -l new_cannot_migrate -l old_is_full -l not_full_cannot_be_assimilated -l assimilated_nothing_to_migrate -l one_thread_at_init_state_after_migration -l upper_strengthening_1 -l upper_strengthening_2 -l paid_tax_0 -l paid_tax_1 -l posted_0 -l posted_1 -l did_not_pay_ub  -l upper_strengthening_3  -l out_of_mem -l revenue_1 -l revenue_2 -l revenue_3 
 upper_global :  LEMMA
       system |- G( (FORALL (i: table_index): i < N => ht[i].num_entries <= (THRESHOLD * table_size(i)) + NUM_THREADS));
 		   
 
 % PROVED  
-% sal-inf-bmc -i -ice -v 1 -d 1 -s yices2 {0} upper -l pending_lb -l old_is_full -l num_to_migrate_and_assimilated_in_old_tables -l upper_strengthening_1 -l posted_1 -l did_not_pay_ub  -l revenue_2 -l revenue_4 
+% sal-inf-bmc -i -ice -v 1 -d 1 -s yices2 {0} upper -l pending_lb -l old_is_full -l num_to_migrate_and_assimilated_in_old_tables -l upper_strengthening_1 -l posted_1 -l did_not_pay_ub  -l revenue_2 -l revenue_3 
 upper :  LEMMA
       system |- G( (old < new  AND not ht[old].assimilated  =>  
                     (ht[new].num_entries <= Migrated(ht,old) + (Migrated(ht,old) / T) +  (NUM_THREADS-1) + pending)));
@@ -336,7 +336,7 @@ upper :  LEMMA
 def mk_property(Filename):
     properties = """
 % PROVED (-d 7 required with 7 threads)
-% sal-inf-bmc -i -ice -v 1 -d 7 -s yices2 {0} invariant -l upper -l upper_global -l pending -l table_sizes -l posted_0 -l posted_1 -l did_not_pay_ub -l revenue_2 -l revenue_4  -l upper_strengthening_1 -l out_of_mem
+% sal-inf-bmc -i -ice -v 1 -d 7 -s yices2 {0} invariant -l upper -l upper_global -l pending -l table_sizes -l posted_0 -l posted_1 -l did_not_pay_ub -l revenue_2 -l revenue_3  -l upper_strengthening_1 -l out_of_mem
 invariant : LEMMA system |- G(FORALL (i: table_index): i < old => ht[i].assimilated);
 
 """.format(Filename)
